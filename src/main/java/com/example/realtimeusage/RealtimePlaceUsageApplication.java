72b30ecf65bcac0f6a42967380fa266a458b982d
i960107 <i960107@naver.com>
2023-10-16 17:21:05 +0900

    Init Project
    - spring project 시작
    - dependency 추가

---
+++ src/main/java/com/example/realtimeusage/RealtimePlaceUsageApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimeusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimePlaceUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimePlaceUsageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realtimeusage/RealtimePlaceUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimeusage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimePlaceUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
