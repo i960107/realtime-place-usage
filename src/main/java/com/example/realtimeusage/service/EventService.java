66592c48c712da0a791acf80cce7c09fb2ff9df9
i960107 <i960107@naver.com>
2023-10-26 18:15:10 +0900

    feat: Event 비지니스 로직 구현
    - jpa repository, querydsl함수 사용하도록 교체.
    - transactional 어노테이션 추가하여 트랜잭션 관리.
    - 삭제시 soft delete 가능하도록 event status추가

--- src/main/java/com/example/realtimeusage/service/EventService.java
+++ src/main/java/com/example/realtimeusage/service/EventService.java
@@ -2,20 +2,40 @@ package com.example.realtimeusage.service;
 
 import com.example.realtimeusage.constant.ErrorCode;
 import com.example.realtimeusage.constant.EventStatus;
+import com.example.realtimeusage.domain.Place;
 import com.example.realtimeusage.dto.EventDto;
 import com.example.realtimeusage.exception.GeneralException;
 import com.example.realtimeusage.repository.EventRepository;
+import com.example.realtimeusage.repository.PlaceRepository;
+import com.querydsl.core.types.Predicate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
+    private final PlaceRepository placeRepository;
 
+    @Transactional(readOnly = true)
+    public List<EventDto> getEvents(Predicate predicate) {
+        try {
+            return StreamSupport.stream(eventRepository.findAll(predicate).spliterator(), false)
+                    .map(EventDto::of)
+                    .collect(Collectors.toList());
+        } catch (Exception exception) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, exception);
+        }
+    }
+
+    //테스트 용 임시 함수
     public List<EventDto> getEvents(
             Long placeId,
             String name,
@@ -24,25 +44,71 @@ public class EventService {
             LocalDateTime endDateTime
     ) {
         try {
-            return eventRepository.findBy(placeId, name, eventStatus, startDateTime, endDateTime);
-        }catch (Exception e){
+            return null;
+        } catch (Exception e) {
             throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
         }
     }
 
+    @Transactional(readOnly = true)
     public Optional<EventDto> getEvent(long eventId) {
-        return eventRepository.findEventById(eventId);
+        try {
+            return eventRepository.findById(eventId).map(EventDto::of);
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
 
     public boolean createEvent(EventDto eventDto) {
-        return eventRepository.create(eventDto);
+        try {
+            if (eventDto == null) {
+                return false;
+            }
+            Place place = placeRepository.findById(eventDto.placeId())
+                    .orElseThrow(GeneralException::new);// error대체 필요..
+            eventRepository.save(eventDto.toEntity(place));
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
 
-    public boolean modifyEvent(long eventId, EventDto eventDto) {
-        return eventRepository.update(eventId, eventDto);
+    public boolean modifyEvent(Long eventId, EventDto eventDto) {
+        try {
+            if (eventId == null || eventDto == null) {
+                return false;
+            }
+            if (!eventId.equals(eventDto.id())) {
+                return false;
+            }
+
+            if (eventDto.placeId() != null && placeRepository.findById(eventDto.placeId()).isEmpty()) {
+                return false;
+            }
+            eventRepository.findById(eventId)
+                    .ifPresent(event -> {
+                        event.update(eventDto);
+                        if (eventDto.placeId() != null) {
+                            event.updatePlace(placeRepository.findById(eventDto.placeId()).get());
+                        }
+                    });
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
 
-    public boolean deleteEvent(long eventId) {
-        return eventRepository.delete(eventId);
+    public boolean deleteEvent(Long eventId) {
+        try {
+            if (eventId == null) {
+                return false;
+            }
+            eventRepository.findById(eventId)
+                    .ifPresent(event -> event.updateStatus(EventStatus.DELETED));
+            return true;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
     }
+
 }
