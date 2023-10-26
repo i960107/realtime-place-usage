9f8e1bdc0ae8d48787f25805eaa6bb17d2195ac0
i960107 <i960107@naver.com>
2023-10-26 18:16:21 +0900

    feat: event controller 가 event service 함수 사용하도록 교체
    - 임시 테스트를 위해서 controller에서 데이터를 생성해서 보내주는 것에서 실제 service를 사용해서 datasource에서 가져온 데이터를 넘겨줄 수 있도록 교체

--- src/main/java/com/example/realtimeusage/controller/EventController.java
+++ src/main/java/com/example/realtimeusage/controller/EventController.java
@@ -1,12 +1,18 @@
 package com.example.realtimeusage.controller;
 
-import com.example.realtimeusage.constant.EventStatus;
+import com.example.realtimeusage.domain.Event;
 import com.example.realtimeusage.dto.EventResponse;
-import java.time.LocalDateTime;
-import java.util.List;
+import com.example.realtimeusage.service.EventService;
+import com.querydsl.core.types.Predicate;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,53 +20,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/events")
 @RequiredArgsConstructor
+@Validated
 public class EventController {
-//    private final EventService eventService;
+    private final EventService eventService;
 
     @GetMapping
-    public String events(Model model) {
-        List<EventResponse> events = List.of(
-                EventResponse.of(
-                        1L,
-                        1L,
-                        "오후 운동",
-                        LocalDateTime.of(2021, 1, 1, 13, 0, 0),
-                        LocalDateTime.of(2021, 1, 1, 16, 0, 0),
-                        24,
-                        0,
-                        EventStatus.OPENED,
-                        "마스크 착용 필수"
-                ),
-                EventResponse.of(
-                        2L,
-                        1L,
-                        "오후 운동",
-                        LocalDateTime.of(2021, 1, 1, 13, 0, 0),
-                        LocalDateTime.of(2021, 1, 1, 16, 0, 0),
-                        24,
-                        0,
-                        EventStatus.OPENED,
-                        "마스트 착용 필수"
-                )
+    public String events(Model model, @QuerydslPredicate(root = Event.class) Predicate predicate) {
+        model.addAttribute("events", eventService.getEvents(predicate)
+                .stream().map(EventResponse::from)
+                .collect(Collectors.toList())
         );
-        model.addAttribute("events", events);
         return "/event/index";
     }
 
     @GetMapping("/{eventId}")
-    public String eventDetail(@PathVariable Long eventId, Model model) {
-        EventResponse event = EventResponse.of(
-                1L,
-                1L,
-                "오후 운동",
-                LocalDateTime.of(2021, 1, 1, 13, 0, 0),
-                LocalDateTime.of(2021, 1, 1, 16, 0, 0),
-                0,
-                24,
-                EventStatus.OPENED,
-                "마스트 착용 필수"
-        );
-        model.addAttribute("event", event);
+    public String eventDetail(
+            @Positive @NotNull @PathVariable Long eventId,
+            Model model) {
+        model.addAttribute("event", eventService.getEvent(eventId).map(EventResponse::from));
         return "/event/detail";
     }
 }
