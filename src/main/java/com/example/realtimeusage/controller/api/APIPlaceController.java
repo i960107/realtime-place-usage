784cd8779f65e6c9672c85c0d1da86b4d64c4362
i960107 <i960107@naver.com>
2023-11-01 20:50:23 +0900

    chore: API 컨트롤러 비활성화
    - rest hal api로 대체

--- src/main/java/com/example/realtimeusage/controller/api/APIPlaceController.java
+++ src/main/java/com/example/realtimeusage/controller/api/APIPlaceController.java
@@ -5,16 +5,13 @@ import com.example.realtimeusage.dto.APIDataResponse;
 import com.example.realtimeusage.dto.PlaceDto;
 import com.example.realtimeusage.dto.PlaceResponse;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 @Deprecated
 //@RestController
@@ -46,12 +43,15 @@ public class APIPlaceController {
             return APIDataResponse.empty();
         }
         return APIDataResponse.of(PlaceDto.of(
+                1L,
                 PlaceType.COMMON,
                 "배드민턴장",
                 "서울시 강남구",
                 "010-1234-5678",
                 30,
-                "신장개업")
+                "신장개업",
+                null,
+                null)
         );
     }
 
